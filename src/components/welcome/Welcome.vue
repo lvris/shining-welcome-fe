3b467a76bebf8b54edc99202878957dd80db6e80
lvris <lvris@qq.com>
2023-04-15 01:56:27 +0800

    feat: initial form structure

--- src/components/welcome/Welcome.vue
+++ src/components/welcome/Welcome.vue
@@ -1,30 +1,60 @@
 <template>
   <el-form
-    :label-position="labelPosition"
-    label-width="100px"
-    :model="formLabelAlign"
-    style="max-width: 460px"
+    label-position="top"
+    size="large"
+    :model="form"
+    :rules="rules"
+    :require-asterisk-position="right"
+    status-icon
   >
-    <el-form-item label="Name">
-      <el-input v-model="formLabelAlign.name" />
+    <el-form-item label="姓名" prop="name">
+      <el-input v-model="form.name" />
     </el-form-item>
-    <el-form-item label="Activity zone">
-      <el-input v-model="formLabelAlign.region" />
+    <el-form-item label="电话号码" prop="phone">
+      <el-input v-model="form.phone" />
     </el-form-item>
-    <el-form-item label="Activity form">
-      <el-input v-model="formLabelAlign.type" />
+    <el-form-item label="就读学校" prop="school">
+      <el-input v-model="form.school" />
+    </el-form-item>
+    <el-form-item label="COS 角色" prop="cos">
+      <el-input v-model="form.cos" />
     </el-form-item>
   </el-form>
 </template>
 
-<script setup>
+<script lang="ts" setup>
 import { reactive, ref } from 'vue';
+import type { FormInstance, FormRules } from 'element-plus';
+import { right } from '@popperjs/core';
 
-const labelPosition = ref('right')
-
-const formLabelAlign = reactive({
+const form = reactive({
+  phone: '',
   name: '',
-  region: '',
-  type: '',
+  school: '',
+  cos: '',
+})
+
+const rules = reactive<FormRules>({
+  name: [
+    { 
+      required: true,
+      message: '请输入姓名',
+      trigger: 'blur',
+    }
+  ],
+  phone: [
+    { 
+      required: true,
+      message: '请输入手机号码',
+      trigger: 'blur',
+    }
+  ],
+  school: [
+    { 
+      required: true,
+      message: '请输入学校名称',
+      trigger: 'blur',
+    }
+  ],
 })
 </script>
