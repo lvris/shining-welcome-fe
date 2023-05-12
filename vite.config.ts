30168898a6871c54a5c8892c88ca4114652b59a7
lvris <lvris@qq.com>
2023-05-12 15:21:52 +0800

    chore: dev_server

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,9 @@ const pathSrc = path.resolve(__dirname, 'src')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3333
+  },
   resolve: {
     alias: {
       '~/': `${pathSrc}/`,
